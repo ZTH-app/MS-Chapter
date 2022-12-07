b25c858dc82f1cca104cbfb355f7b4f140f7abc0
CloeBlanchard <cloe.blanchard83@gmail.com>
2022-12-07 16:10:25 +0100

    remove import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
