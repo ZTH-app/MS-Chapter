399e762b678aae1b4d15f75a5834120d3ce66115
0h My Goat <daftouney@gmail.com>
2022-12-08 14:42:07 +0100

    fix: url mongo :bug:

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChapterModule } from './chapter/chapter.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: 'nest.env',
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-  ChapterModule
-],
-  
+    ChapterModule,
+  ],
 })
 export class AppModule {}
--- src/chapter/chapter.module.ts
+++ src/chapter/chapter.module.ts
@@ -1,15 +1,15 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ChapterController } from "./chapter.controller";
-import  { Chapter, ChapterSchema } from "./schemas/chapter.schema"
-import { ChapterService } from "./chapter.service";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChapterController } from './chapter.controller';
+import { Chapter, ChapterSchema } from './schemas/chapter.schema';
+import { ChapterService } from './chapter.service';
+import { ChapterRepository } from './repo/chapter.repo';
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{ name: Chapter.name, schema: ChapterSchema}]),
-    ],
-    providers: [ChapterService],
-    controllers: [ChapterController],
+  imports: [
+    MongooseModule.forFeature([{ name: Chapter.name, schema: ChapterSchema }]),
+  ],
+  providers: [ChapterService, ChapterRepository],
+  controllers: [ChapterController],
 })
-
 export class ChapterModule {}
